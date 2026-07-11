a757de893944ef9bbf16c1f0650fd01c9fc32524
mounikabollimpalli <mounikabollimpalli@gmail.com>
2026-07-11 13:22:14 +0530

    Auto seed database on startup

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Change this if your backend runs elsewhere
-export const API_BASE_URL = 'http://localhost:8000'
+export const API_BASE_URL = 'https://banking-assistant-ba9h.onrender.com'
 
 const api = axios.create({ baseURL: API_BASE_URL })
 
